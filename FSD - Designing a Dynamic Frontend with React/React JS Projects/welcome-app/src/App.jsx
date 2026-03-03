e7456f86128fba9fabdc72acdee81d9bf59f98c0
Akash <akash300383@gmail.com>
2026-03-03 08:13:11 +0530

    done

--- FSD - Designing a Dynamic Frontend with React/React JS Projects/welcome-app/src/App.jsx
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/welcome-app/src/App.jsx
@@ -1,15 +1,39 @@
-function App(){
 
+function HeaderComponent() {
+  return <h2>This is my Header Component</h2>
+}
+
+function FooterComponent() {
+  return <h2>This is my Footer Component</h2>
+}
+
+function ButtonComponent() {
+  return <button>Click Me</button>
+}
+
+function EmployeeComponent() {
+  let name = "Akash";       // JS Code 
+
+  return(
+    <div>
+        <p>My Name is {name}</p>
+    </div>
+  )
+}
+function App(){
 
   //return <div>Welcome to React JS created by Akash</div>
   // return <div>Welcome to React JS</div>
   //       <p>Welcome to React JS once again</p>
 
-  
   return(
     <div>
+      <HeaderComponent></HeaderComponent>
       <h1>Welcome to React JS</h1>
       <p>Today we are learning React JS</p>
+      <EmployeeComponent></EmployeeComponent>
+      <ButtonComponent></ButtonComponent>
+      <FooterComponent></FooterComponent>
     </div>
   )
 
