28df49e59645f316ae2a625477132e38137f7384
Akash <akash300383@gmail.com>
2026-03-03 07:28:45 +0530

    done

---
+++ FSD - Designing a Dynamic Frontend with React/React JS Projects/welcome-app/src/App.jsx
@@ -0,0 +1,18 @@
+function App(){
+
+
+  //return <div>Welcome to React JS created by Akash</div>
+  // return <div>Welcome to React JS</div>
+  //       <p>Welcome to React JS once again</p>
+
+  
+  return(
+    <div>
+      <h1>Welcome to React JS</h1>
+      <p>Today we are learning React JS</p>
+    </div>
+  )
+
+}
+
+export default App;
